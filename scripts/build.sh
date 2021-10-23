5966a349c5db841ef36facbd570007ad74a0fe25
Harrm <abrehchs@gmail.com>
2021-10-23 13:14:50 +0300

    Fix typo in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,16 +10,16 @@ cd "$(dirname "$0")/.."
 
 git submodule update --init
 
-CMAKE_ARGS=( "-B\"${BUILD_DIR}\"" "${@[@]}" )
+CMAKE_ARGS=( "-B\"${BUILD_DIR}\"" "$@" )
 
 # see clang-tidy build in ci.yml github workflow
 if [ "${BUILD_TARGET}" == "test" ]; then
   CMAKE_ARGS+=( "-DBUILD_TESTS=ON" )
 fi
-cmake . "${CMAKE_ARGS[*]}"
+echo "${CMAKE_ARGS[*]}" | xargs cmake .
 
 if [ "${BUILD_TARGET}" == "test" ]; then
   # first build test executables
-  cmake --build "${BUILD_DIR}" -- "${MAKE_ARGS}"
+  echo "${MAKE_ARGS}" | xargs cmake --build "${BUILD_DIR}" --
 fi
-cmake --build "${BUILD_DIR}" --target "${BUILD_TARGET}" -- "${MAKE_ARGS}"
+echo "${MAKE_ARGS}" | xargs cmake --build "${BUILD_DIR}" --target "${BUILD_TARGET}" --
